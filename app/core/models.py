e7cd5a83aca591c025cad8022234191d6422b497
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2020-04-12 11:25:03 +0000

    Added the models and test cases

--- app/core/models.py
+++ app/core/models.py
@@ -1,28 +1,27 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+    	"""Creates and saves a new User"""
+    	if not email:
+        	raise ValueError('Users must have an email address')
 
-        """Creates and saves a new User"""
-	if not email:
-	    raise ValueError('Users must have an email address")
-        user = self.model(email=self.normalize_email(email), **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
+    	user = self.model(email=self.normalize_email(email), **extra_fields)
+    	user.set_password(password)
+    	user.save(using=self._db)
 
-        return user
+    	return user
 
     def create_superuser(self, email, password):
-	"""Creates and saves a new super user"""
-	user = self.create_user(email, password)
-	user.is_staff = True
-	user.is_superuser = True
-	user.save(using=self._db)
+    	"""Creates and saves a new super user"""
+    	user = self.create_user(email, password)
+    	user.is_staff = True
+    	user.is_superuser = True
+    	user.save(using=self._db)
 
-	return user
+    	return user
 
 class User(AbstractBaseUser, PermissionsMixin):
 
