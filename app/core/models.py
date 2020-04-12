508d3daed8cf20de714c47394db38dee3e2348b6
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2020-04-12 11:55:54 +0000

    Corrected the tab indentation issue and tab issue

--- app/core/models.py
+++ app/core/models.py
@@ -1,31 +1,32 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
+
 
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
-    	"""Creates and saves a new User"""
-    	if not email:
-        	raise ValueError('Users must have an email address')
+        """Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users must have an email address')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-    	user = self.model(email=self.normalize_email(email), **extra_fields)
-    	user.set_password(password)
-    	user.save(using=self._db)
-
-    	return user
+        return user
 
     def create_superuser(self, email, password):
-    	"""Creates and saves a new super user"""
-    	user = self.create_user(email, password)
-    	user.is_staff = True
-    	user.is_superuser = True
-    	user.save(using=self._db)
+        """Creates and saves a new super user"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-    	return user
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
-
-    """Custom user model that supports using email instead of username"""
+    """Custom user model that suppors using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
