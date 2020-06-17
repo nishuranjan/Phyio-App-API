9ec41873e4e7b977f7d497798af55333865c4a6b
Ubuntu <ubuntu@ip-172-31-90-178.ec2.internal>
2020-06-17 07:59:25 +0000

    AIT-7 Updated the tag lists

--- app/app/urls.py
+++ app/app/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('user.urls')),
-    # path('api/recipe/', include('recipe.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
 # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- app/core/models.py
+++ app/core/models.py
@@ -50,3 +50,21 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+   # ingredients = models.ManyToManyField('Ingredient')
+    tags = models.ManyToManyField('Tag')
+   # image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self):
+        return self.title
